8cc2436ca3efcb3fd46627dd4457162c82b0174b
Efe <efetwkin@gmail.com>
2026-04-01 10:50:50 +0200

    Infopagina map aangemaakt

---
+++ infopagina/index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Infopagina</title>
+</head>
+<body>
+
+<header>
+    <nav></nav>
+    <h1>Test</h1>
+</header>
+
+<main>
+    <h2></h2>
+    <p></p>
+</main>
+
+<footer></footer>
+
+</body>
+</html>
