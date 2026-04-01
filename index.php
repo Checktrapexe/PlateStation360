7d6cf022c4655a4576a6402478efeb50b5771405
Checktrapexe <chrisvanloenen7@gmail.com>
2026-04-01 14:17:12 +0200

    index.php updated met meer informatie en css verder aangevult

--- index.php
+++ index.php
@@ -12,8 +12,37 @@
 <nav>
     <?php require_once "nav.php"; ?>
 </nav>
+<header>
+    <div class="headerText">
+        <h1>Het beste toegankelijke bord ter wereld</h1>
+        <p>De PlateStation 360® is een gebruiksvriendelijke, draaibaar bord, ontworpen om toegankelijk te zijn.</p>
+    </div>
+    <div>
+        <img src="media/images/Placeholder.png" alt="Placeholder image">
+    </div>
+</header>
 <main>
 
+    <section>
+        <article>
+            <img src="media/images/Placeholder.png" alt="Placeholder image">
+            <h2>Placeholder</h2>
+            <p>Lorem Ipsum</p>
+            <a href="">Meer info</a>
+        </article>
+        <article>
+            <img src="media/images/Placeholder.png" alt="Placeholder image">
+            <h2>Placeholder</h2>
+            <p>Lorem Ipsum</p>
+            <a href="">Meer info</a>
+        </article>
+        <article>
+            <img src="media/images/Placeholder.png" alt="Placeholder image">
+            <h2>Placeholder</h2>
+            <p>Lorem Ipsum</p>
+            <a href="">Meer info</a>
+        </article>
+    </section>
 </main>
 <footer>
     <?php require_once "footer.php"; ?>
