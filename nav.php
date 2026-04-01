88c5a23e00b3a6e5c0489fdc97f8f1986b05a6bd
Checktrapexe <chrisvanloenen7@gmail.com>
2026-04-01 10:42:51 +0200

    Nav Setup van Index

---
+++ nav.php
@@ -0,0 +1,10 @@
+<div>
+    <a href="index.php"><img src="media/images/PSlogo.png" alt="Logo that says PlateStation 360 at a slight angle"></a>
+</div>
+
+<div>
+    <a href="index.php">Home</a>
+    <a href="">De PlateStation</a>
+    <a href="">Over ons</a>
+    <a href="">Contact</a>
+</div>
