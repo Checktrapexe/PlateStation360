5e923932cfc13c2026785c8e4dc1be45f5786255
Checktrapexe <chrisvanloenen7@gmail.com>
2026-04-02 14:42:59 +0200

    style fix

--- overons.php
+++ overons.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>PlateStation360 - Over Ons</title>
     <link rel="stylesheet" href="css/styleoverons.css"/>
+    <style>
+        @import url(https://db.onlinewebfonts.com/c/457055c70a9c98aa64002f05d6652edc?family=ITC+Eras+Demi);
+        @import url(https://www.dropbox.com/scl/fi/ctijlfue8498st43rt2sq/Alphazet-SemiBold.ttf?rlkey=b8kbprn901h9ezlhh02zlwdr7&st=n1sr65w7&dl=1);
+    </style>
 </head>
 <body>
 
