f93b7c6fffd696d6b79c80c7fa0523684b8793ec
Checktrapexe <chrisvanloenen7@gmail.com>
2026-04-02 12:55:27 +0200

    nav fix met link

--- nav.php
+++ nav.php
@@ -5,8 +5,7 @@
 
 <div class="navLinks">
     <a href="index.php">Home</a>
-    <a href="">PS 360</a>
+    <a href="infopagina/info.php">PS 360</a>
     <a href="">Over ons</a>
     <a href="">Contact</a>
-    <a href="infopagina/info.php">Informatie</a>
 </div>
