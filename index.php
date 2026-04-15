2cf4b53de121298f812430e7f6723ab151a0a7bb
Eren <erenutu9@gmail.com>
2026-04-15 12:59:28 +0200

    Tekst homepagina

--- index.php
+++ index.php
@@ -18,21 +18,23 @@
 </nav>
 
 <header>
-
     <div>
         <h1>De PlateStation 360</h1>
-        <p>Aliquam erat volutpat. Pellentesque mollis ante in vestibulum gravida. Nullam elementum augue non fermentum
-            tempor. Cras sit amet diam id orci porttitor tempor. Fusce ultrices sed augue nec tempor. Praesent pharetra
-            ornare lacus tincidunt ultrices. Vivamus facilisis odio ligula. Cras interdum nunc a lorem lobortis, vel
-            rhoncus
-            libero malesuada</p>
+        <p>
+            Welkom bij de centrale projectpagina van Team 13 voor CLE 3. Binnen onze casus
+            <em>uit eten gaan met een beperking</em> ontwerpen en ontwikkelen we een oplossing
+            voor Charlotte, een jonge vrouw met polio die gebruikmaakt van een rolstoel.
+            Ons concept, de PlateStation360, is een interactief object met een Circuit Playground
+            dat een bord met eten kan ronddraaien wanneer het eten buiten bereik ligt.
+            Hiermee willen we de eetervaring toegankelijker, zelfstandiger en comfortabeler maken.
+        </p>
         <div class="buttons">
             <a href="info.php" class="btn btn-primary">Meer Info</a>
-            <a href="#" class="btn btn-secondary">Koop Platestation</a>
+            <a href="#" class="btn btn-secondary">Bekijk het Concept</a>
         </div>
     </div>
     <div>
-        <img src="media/images/platestaion%20base.png" alt="">
+        <img src="media/images/platestaion%20base.png" alt="PlateStation 360 basis">
     </div>
 </header>
 
@@ -41,47 +43,49 @@
     <section class="midMain">
         <div class="midContainer">
 
-
             <div class="midImage">
-                <img src="media/images/platestationfood2.png" alt="PlateStation food">
+                <img src="media/images/platestationfood2.png" alt="PlateStation met eten">
             </div>
 
-
             <div class="midText">
-                <h2>Ultiem Comfort</h2>
+                <h2>Meer zelfstandigheid aan tafel</h2>
                 <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                    De PlateStation360 is ontworpen om mensen met een fysieke beperking
+                    meer controle te geven tijdens het eten. Wanneer een deel van de maaltijd
+                    lastig bereikbaar is, kan het bord eenvoudig worden gedraaid zodat het eten
+                    weer binnen handbereik komt.
                 </p>
                 <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Door technologie en gebruiksgemak te combineren in één interactief product,
+                    biedt de PlateStation360 een praktische oplossing die bijdraagt aan comfort,
+                    zelfstandigheid en een prettigere ervaring tijdens het uit eten gaan.
                 </p>
             </div>
 
         </div>
     </section>
 
-
     <section class="bottomMain">
         <div class="bottomContainer">
 
-
             <div class="bottomText">
-                <h3>Tijdloos Design</h3>
+                <h3>Toegankelijk, slim en comfortabel</h3>
                 <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                    Met de PlateStation360 willen we bijdragen aan een inclusievere eetervaring.
+                    Ons ontwerp richt zich niet alleen op functionaliteit, maar ook op eenvoud,
+                    comfort en een moderne uitstraling. Zo ontwikkelen we een concept dat niet
+                    alleen ondersteunt in het dagelijks leven, maar ook aansluit bij de wensen
+                    van de gebruiker.
                 </p>
 
                 <div class="buttons">
-                    <a href="#" class="secondary">Koop Platestation</a>
+                    <a href="#" class="secondary">Bekijk het Prototype</a>
                     <a href="contact.php" class="tertiary">Contact</a>
                 </div>
             </div>
 
-
             <div class="bottomImage">
-                <img src="media/images/platestationeating.png" alt="Dinner with PlateStation">
+                <img src="media/images/platestationeating.png" alt="Gebruiker eet met PlateStation">
             </div>
 
         </div>
